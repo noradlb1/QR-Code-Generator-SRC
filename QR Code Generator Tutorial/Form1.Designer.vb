6a82058c31e530cb5e93ed2743a6c71e10d0f7ec
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-28 16:09:33 +0100

    QR-Code-Generator-SRC
    QR-Code-Generator-SRC

---
+++ QR Code Generator Tutorial/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.PictureBoxLEER = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBoxLEER = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxLEER, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(17, 152)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(261, 245)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(17, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(261, 134)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button1.Location = New System.Drawing.Point(17, 403)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(261, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.FileName = "QR_Code.png"
+        Me.SaveFileDialog1.Filter = "PNG Image|*.png"
+        '
+        'PictureBoxLEER
+        '
+        Me.PictureBoxLEER.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBoxLEER.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBoxLEER.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxLEER.Location = New System.Drawing.Point(329, 152)
+        Me.PictureBoxLEER.Name = "PictureBoxLEER"
+        Me.PictureBoxLEER.Size = New System.Drawing.Size(261, 245)
+        Me.PictureBoxLEER.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBoxLEER.TabIndex = 3
+        Me.PictureBoxLEER.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button2.Location = New System.Drawing.Point(329, 403)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(261, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Open"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBoxLEER
+        '
+        Me.TextBoxLEER.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBoxLEER.ForeColor = System.Drawing.Color.Black
+        Me.TextBoxLEER.Location = New System.Drawing.Point(329, 12)
+        Me.TextBoxLEER.Multiline = True
+        Me.TextBoxLEER.Name = "TextBoxLEER"
+        Me.TextBoxLEER.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxLEER.Size = New System.Drawing.Size(261, 134)
+        Me.TextBoxLEER.TabIndex = 104
+        Me.TextBoxLEER.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(607, 433)
+        Me.Controls.Add(Me.TextBoxLEER)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBoxLEER)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "QR Code Generator Tutorial"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxLEER, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents PictureBoxLEER As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBoxLEER As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+End Class
---
+++ QR Code Generator Tutorial/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports ZXing
+
+Public Class Form1
+    Dim QR_Generator As New MessagingToolkit.QRCode.Codec.QRCodeEncoder
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            PictureBox1.Image = QR_Generator.Encode(TextBox1.Text)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SaveFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+        Try
+            Dim img As New Bitmap(PictureBox1.Image)
+            img.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Png)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If OpenFileDialog1.ShowDialog Then
+            Try
+                TextBoxLEER.Clear()
+                PictureBoxLEER.ImageLocation = OpenFileDialog1.FileName
+                ESPERA(1000)
+                Dim DECODER As BarcodeReader = New BarcodeReader
+                TextBoxLEER.Text = DECODER.Decode(PictureBoxLEER.Image).ToString
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+
+
+    Public Sub ESPERA(ByVal INTERVALO As Integer)
+        Dim PARADA As New Stopwatch
+        PARADA.Start()
+        Do While PARADA.ElapsedMilliseconds < INTERVALO
+            Application.DoEvents()
+        Loop
+        PARADA.Stop()
+    End Sub
+End Class
